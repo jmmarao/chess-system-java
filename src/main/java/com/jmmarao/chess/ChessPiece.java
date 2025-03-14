53d80bd6380741b492573cd83ac28305160ed1eb
jones <jmmarao@gmail.com>
2025-03-14 14:41:48 -0300

    Piece move count

--- src/main/java/com/jmmarao/chess/ChessPiece.java
+++ src/main/java/com/jmmarao/chess/ChessPiece.java
@@ -6,6 +6,7 @@ import com.jmmarao.boardgame.Position;
 
 public abstract class ChessPiece extends Piece {
     private Color color;
+    private int moveCount;
 
     public ChessPiece(Board board, Color color) {
         super(board);
@@ -17,11 +18,23 @@ public abstract class ChessPiece extends Piece {
         return p != null && p.getColor() != color;
     }
 
+    public void increaseMoveCount() {
+        moveCount++;
+    }
+
+    public void decreaseMoveCount() {
+        moveCount--;
+    }
+
     public ChessPosition getChessPosition() {
         return ChessPosition.fromPosition(position);
     }
 
     public Color getColor() {
         return color;
+    }
+
+    public int getMoveCount() {
+        return moveCount;
     }
 }
