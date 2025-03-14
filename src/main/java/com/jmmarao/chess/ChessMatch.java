81564e68eb4eec525badb5d7690c25fccf0a77f9
jones <jmmarao@gmail.com>
2025-03-14 15:11:33 -0300

    Bishop

--- src/main/java/com/jmmarao/chess/ChessMatch.java
+++ src/main/java/com/jmmarao/chess/ChessMatch.java
@@ -3,6 +3,7 @@ package com.jmmarao.chess;
 import com.jmmarao.boardgame.Board;
 import com.jmmarao.boardgame.Piece;
 import com.jmmarao.boardgame.Position;
+import com.jmmarao.chess.pieces.Bishop;
 import com.jmmarao.chess.pieces.King;
 import com.jmmarao.chess.pieces.Pawn;
 import com.jmmarao.chess.pieces.Rook;
@@ -201,6 +202,8 @@ public class ChessMatch {
 
     private void initialSetup() {
         placeNewPiece('a', 1, new Rook(board, Color.WHITE));
+        placeNewPiece('c', 1, new Bishop(board, Color.WHITE));
+        placeNewPiece('f', 1, new Bishop(board, Color.WHITE));
         placeNewPiece('e', 1, new King(board, Color.WHITE));
         placeNewPiece('h', 1, new Rook(board, Color.WHITE));
         placeNewPiece('a', 2, new Pawn(board, Color.WHITE));
@@ -213,6 +216,8 @@ public class ChessMatch {
         placeNewPiece('h', 2, new Pawn(board, Color.WHITE));
 
         placeNewPiece('a', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('c', 8, new Bishop(board, Color.BLACK));
+        placeNewPiece('f', 8, new Bishop(board, Color.BLACK));
         placeNewPiece('e', 8, new King(board, Color.BLACK));
         placeNewPiece('h', 8, new Rook(board, Color.BLACK));
         placeNewPiece('a', 7, new Pawn(board, Color.BLACK));
